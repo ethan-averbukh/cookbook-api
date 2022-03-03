57b26d800e2455b411c8b5eb5f7b064f191b1e00
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-03-03 11:54:10 -0700

    initial commit of server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const PORT = process.env.PORT || 4000;
 
 
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+app.use(cors());
 
 // Add the middleware code needed to accept incoming data and add it to req.body
 
